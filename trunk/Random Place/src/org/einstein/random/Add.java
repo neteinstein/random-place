9c1107c0231cc0d16dfd6d44b055f466a208a4db
neteinstein@gmail.com <neteinstein@gmail.com@6e69c2ed-57e3-ca30-f982-bd3de617a8dc>
2010-10-04 19:15:25 +0000

    + Application starts. + First two screens start to take shape.

---
+++ trunk/Random Place/src/org/einstein/random/Add.java
@@ -0,0 +1,13 @@
+package org.einstein.random;
+
+import android.app.Activity;
+
+public class Add extends Activity{
+
+	protected void onStart() {
+        super.onStart();
+
+        setContentView(R.layout.add);
+	}
+
+}
---
+++ trunk/Random Place/src/org/einstein/random/RandomNear.java
@@ -0,0 +1,12 @@
+package org.einstein.random;
+
+import android.app.Activity;
+
+public class RandomNear extends Activity{
+
+	protected void onStart() {
+        super.onStart();
+
+        setContentView(R.layout.random);
+	}
+}
